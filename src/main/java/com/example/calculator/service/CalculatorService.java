f0be15df6e71d3e6ac64d2059f0801643cd9191a
Ahmodiyy <ahmodolaitan03@gmail.com>
2025-12-01 16:36:39 +0100

    hazelcast dependency and code

--- src/main/java/com/example/calculator/service/CalculatorService.java
+++ src/main/java/com/example/calculator/service/CalculatorService.java
@@ -1,5 +1,6 @@
 package com.example.calculator.service;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -7,7 +8,13 @@ public class CalculatorService {
 
     final static double PI = 3.142;
 
+    @Cacheable("sum")
     public int sum(int a, int b) {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         return a + b;
     }
 
